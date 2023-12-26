23b12e7f5ae1e601a9de5e742256757eef798f79
VazquezAlex <alejandrovazquez.dev@gmail.com>
2023-12-26 17:18:16 -0600

    feature: added page header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import { Col } from 'antd';
 import PokemonList from './components/PokemonList';
 import Searcher from './components/Searcher';
+import logo from './statics/logo.svg';
 import './App.css';
 
 function App() {
     return (
         <div className="App">
+            <Col span = { 4 } offset = { 10 }>
+                <img src = {logo} alt = 'Pokedux' />
+            </Col>
             <Col span = { 8 } offset = { 8 }>
                 <Searcher />
             </Col>
