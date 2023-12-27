be710078bd0fe8d357639956e374fe72e9646b34
VazquezAlex <alejandrovazquez.dev@gmail.com>
2023-12-26 23:04:00 -0600

    feature: connected react-redux to the project

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import { useEffect, useState } from 'react';
+import { connect } from 'react-redux';
 import { Col } from 'antd';
 import { getPokemons } from './api';
 import PokemonList from './components/PokemonList';
 import Searcher from './components/Searcher';
 import logo from './statics/logo.svg';
 import './App.css';
+import { setPokemons as setPokemonsActions } from './actions';
 
-function App() {
 
-    const [pokemons, setPokemons] = useState([]);
+function App({ pokemons, setPokemons }) {
 
     useEffect(() => {
         const fetchPokemons = async () => {
@@ -32,4 +33,12 @@ function App() {
     );
 }
 
-export default App;
+const mapStateToProps = (state) => ({
+    pokemons: state.pokemons,
+});
+
+const mapDispatchToProps = (dispatch) => ({
+    setPokemons: (values) => dispatch(setPokemonsActions(values))
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
